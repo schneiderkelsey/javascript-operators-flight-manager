a18985e15224820db880e1b61f5a1ebd0fd1ea26
schneiderkelsey <kelsey.schneider1988@gmail.com>
2021-11-08 19:32:04 -0500

    Completed module

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,22 @@
 function Passengers() {
+    function checkFlightCapacity(capacity, passengers) {
+        let numPassengers = 0; 
+        for (let i = 0; i < passenfers.length; i++) {
+            numPassengers += passengers[i];
+        }
+   //If the total number of passengers is less than the capacity of the flight, return it. 
+   if (numPassengers <= capacity) { 
+       return numPassengers;
+   }
 
+   //If there are more passengers than the flight capacity, you should throw an error. 
+   if (numPassengers > capacity) {
+       throw new Error("There are more passengers than the flight capacity"); 
+    }
 }
+
+return { checkFlightCapacity }; 
+}
+
+module.exports = Passengers();
 
