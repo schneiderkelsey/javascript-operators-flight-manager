51674f7b141e32e144c9f29fdfe7b823c789753a
schneiderkelsey <kelsey.schneider1988@gmail.com>
2021-11-11 17:56:49 -0500

    Completed module

--- logic/util.js
+++ logic/util.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function Util() {
 
     function calculateTotalDistributedPassengers(distributedPassengers) {
@@ -18,7 +20,36 @@ function Util() {
         return totalNumberOfPassengers;
      }
 
-     return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
+     function checkInput(input) {
+      if (!input) {
+          throw new Error("Incorrect values. Check all input fields to be filled in.");
+      }
+      if (isNaN(input)) {
+          throw new Error("Incorrect values. Check all input fields to be numbers.");
+      }
+  }
+
+  function calculateTotalDistance(distancesArray) {
+      let totalDistance = 0;
+      let distance;
+      for (distance of distancesArray) {
+         if (distance < 0) {
+            continue;
+         }
+         totalDistance += distance;
+      }
+      return totalDistance;
+   }
+
+   function calculateBonusPoints(businessDistancesArray, economyDistancesArray, businessBonus, economyBonus) {
+      let totalBusinessDistance = calculateTotalDistance(businessDistancesArray);
+      let totalEconomyDistance = calculateTotalDistance(economyDistancesArray);
+      let points = (businessBonus * totalBusinessDistance)/100 + (economyBonus * totalEconomyDistance)/100;
+      return points;
+   }
+
+   return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers, checkInput, 
+          calculateTotalDistance, calculateBonusPoints};
 
 }
 
