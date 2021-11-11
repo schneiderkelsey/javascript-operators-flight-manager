71dd26694e4a287a3c2f86a591a31e4fd6aa6be8
schneiderkelsey <kelsey.schneider1988@gmail.com>
2021-11-11 17:47:51 -0500

    Completed module

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,21 @@
 function Util() {
-
+    function calculateTotalDistributedPassengers(distributePassengers) {
+        let totalDistributedPassengers = 0;
+        let value; 
+        for (value in distributedPassengers) { 
+            totalDistributedPassengers += distributedPassengers[value];
+        }
+        return totalDistributedPassengers;
+    }
+    function calculateTotalNumberOfPassengers(passengersArray) { 
+        let totalNumberOfPassengers = 0;
+        let passengers;
+        for (passengers of passengersArray) {
+            totalNumberOfPassengers += passengers;
+        }
+        return totalNumberOfPassengers;
+    }
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers}; 
 }
+module.exports = Util();
 
